271d763a0cfee7c9d7a897f7615f80dee011a742
withcoffee-code <withcoffee@gmail.com>
2026-02-04 12:44:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,8 @@ st.markdown(
      #   text-align: right;
         font-weight: 700;
         font-size: 1.4rem;
-        margin-top: 0.35rem;
-        margin-right: 3rem;
+    #    margin-top: 0.35rem;
+    #    margin-right: 3rem;
         color: rgb(130, 30, 80);
       }}
       .right-total small {{
